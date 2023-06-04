9a5693ea9faf8e09d14ff0b03eafa7b25ba48d31
Beni Vucsics <vucben99@gmail.com>
2023-06-04 20:43:34 +0200

    .

--- src/api/getCurrentWeather.js
+++ src/api/getCurrentWeather.js
@@ -1,11 +1,10 @@
 import axios from "axios"
-const k = "a1533fbd4b9bce5cd4d92ac37617727d"
 
 async function getCurrentWeather(capital) {
   try {
     const city = capital.split("-")[0]
     const country = capital.split("-")[1]
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${k}&units=metric`)
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=a1533fbd4b9bce5cd4d92ac37617727d&units=metric`)
     return response.data
   } catch {
     return {}
